a304f55dfa4ece82fa6f0ce27b8f8c5341e0cda1
Louis <koo2434@gmail.com>
2018-11-15 17:46:43 -0500

    Create DataValidation.java

---
+++ Zwaste/app/src/main/java/cs/gtstudent/zwaste/DataValidation.java
@@ -0,0 +1,45 @@
+package cs.gtstudent.zwaste;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DataValidation {
+    public static boolean isValidEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        if (email.matches(emailRegex)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static String isValidPassword (String password) {
+
+        if (password.length() < 8) {
+            return "Password must be at least 8 characters long.";
+        }
+
+        String regex = "[!@#$%^&*(),.?\":{}|<>]";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        if (!matcher.find()) {
+            return "Password must contain at least one special character.";
+        }
+
+        regex = "[0-9]";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(password);
+        if (!matcher.find()) {
+            return "Password must contain at least one number.";
+        }
+
+        regex = "(.*[A-Z].*)";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(password);
+        if (!matcher.find()) {
+            return "Password must contain at least one capital letter.";
+        }
+
+        return "Valid Password.";
+    }
+}
